4fc4a0d69e72e11b6b0736775c3b0a790de17287
sitaraanil <2103368C@student.tp.edu.sg>
2022-12-06 16:03:26 +0800

    pushing userservlet

--- src/main/java/UserServlet1.java
+++ src/main/java/UserServlet1.java
@@ -18,13 +18,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class UserServlet
  */
 @WebServlet("/UserServlet")
-public class UserServlet extends HttpServlet {
+public class UserServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public UserServlet() {
+	public UserServlet1() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
